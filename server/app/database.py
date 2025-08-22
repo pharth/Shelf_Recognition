7795c1982550fc472bdf2d1a6d398b97ca50cd73
Parth Hanchate <83768073+pharth@users.noreply.github.com>
2025-08-22 16:01:44 +0530

    using beanie

--- server/app/database.py
+++ server/app/database.py
@@ -1,9 +1,15 @@
 import motor.motor_asyncio
+from beanie import init_beanie
+from models.user import User
 from config import settings
 
 # MongoDB connection
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGODB_URL)
 database = client[settings.DATABASE_NAME]
+
+async def init_db():
+    """Initialize database with Beanie"""
+    await init_beanie(database=database, document_models=[User])
 
 async def get_db():
     return database
